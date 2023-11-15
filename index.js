6feee9cd29075879e9e4dc7dcc0348646d91d008
Ignacio Pascual <ignaciopascualmerino@gmail.com>
2023-11-15 09:27:03 +0100

    commit previo a cambio en una función y gestion de error

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 require("dotenv").config();
 
 const express = require("express");
+const {leerTareas,crearTarea,editarEstadoTarea,editarTextoTarea,borrarTarea} = require("./db");
 
 const servidor = express();
+
+servidor.use("/pruebas-api",express.static("/pruebas_api"));
+
+
+servidor.get("/tareas", async (peticion,respuesta) => {
+    throw "el error que suceda...";
+});
+
+servidor.use((error,peticion,respuesta,siguiente) => {
+    // cualquier exceptcion que envie el sistema (throw) será capturada por este middleware
+    console.log(error);
+    respuesta.send("...ocurrio un error");
+});
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({error : "recurso no encontrado" });
+});
+
 
 
 servidor.listen(process.env.PORT);
